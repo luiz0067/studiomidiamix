724a65381607e399fee2f9f009f44181114ea810
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-09 10:43:36 -0300

    Update conecta.php

--- site/conecta.php
+++ site/conecta.php
@@ -1,1 +1,16 @@
 
+	<?php
+	$host		=	"";
+	$login		=	"";
+	$password	=	"";
+	$base_dados	=	"";
+
+if (!$link = mysql_connect($host, $login, $password)) {
+    echo 'Não foi possível conectar ao mysql';
+    exit;
+}
+if (!mysql_select_db($base_dados, $link)) {
+    echo 'Não foi possível selecionar o banco de dados';
+    exit;
+}
+?>
