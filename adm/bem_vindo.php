9e0757047f23e1094731701d83dbb5b4a5987447
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-09 10:32:21 -0300

    Add files via upload

---
+++ adm/bem_vindo.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Tela de Apresentação Mídia Mix</title>
+
+<style type="text/css">
+	body{
+		width:100%; height:100; margin-left:0px; margin-right:0px; margin-top:0px; margin-bottom:0px;}
+		
+	#tudo{
+		width:100%; height:100; margin-left:0px; margin-right:0px; margin-top:0px; margin-bottom:0px;}	
+		
+	.linhas {
+		width:100%; height:auto; clear:both; display:block;}
+		
+	.linhacentro{
+		width:800px; height:auto; min-height:4px; clear:both; display:block; overflow:hidden; float:center; margin-left:auto; margin-right:auto;}	
+		
+	.espaco{
+		float:left; display:block;}	
+		
+	#ERP{
+		width:533px; height:112px; float:left; display:block; text-align:center; line-height:112px; font-family:Arial, Helvetica, sans-serif; font-style:normal; font-size:24px; color:#666;}
+		
+	@font-face {		
+				font-style: normal;
+				font-weight: normal;
+				font-family:"erp";
+
+				_src: url("./fonts/raleway-thin.eot"),url("./fonts/raleway-thin.otf");
+				src: url("./fonts/raleway-thin.ttf");
+			}			
+</style>
+<script type="text/javascript">
+</script>
+</head>
+
+<body onload="MM_preloadImages('entrar2.gif')">
+	<div id="tudo">
+    	<div class="linhas">
+        	<div class="linhacentro" style="height:91px;"></div>
+        </div>
+        <div class="linhas">
+        	<div class="linhacentro" style="height:151px;">
+            	<div class="espaco" style="height:151px; width:264px;"></div>
+                <div class="espaco" style="height:151px; width:298px; background-image:url(./imagens/logo.jpg)"></div>
+               
+            </div>
+        </div>
+
+   
+            
+</body></html>
